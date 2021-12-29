e7428f7a2b4bc75b848fddc405fe4fb2fadb3c0e
sang5c <skah321@gmail.com>
2021-12-29 23:57:47 +0900

    refactor: 불필요한 테스트 제거

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -1,7 +1,6 @@
 package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,17 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
-    @DisplayName("문자열을 통해 계산식을 생성한다.")
-    @Test
-    void fromString() {
-        assertThat(Calculator.execute("1 + 2 - 3")).isNotNull();
-    }
-
     @DisplayName("계산식을 실행하면 계산된 결과를 반환한다.")
     @ParameterizedTest(name = "{0} = {1}")
     @CsvSource({"1 + 2 - 3, 0", "2 + 3 * 4 / 2, 10"})
     void calculate(String str, int expected) {
-        // Calculator calculator = Calculator.execute(str);
         assertThat(Calculator.execute(str)).isEqualTo(expected);
     }
 
