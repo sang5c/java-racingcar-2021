71937e772abd47234c9b94e1badcda61096d1bce
sang5c <skah321@gmail.com>
2021-12-31 23:31:02 +0900

    feat(main): 사용자로부터 이동을 시도할 횟수, 자동차 수를 받아라

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,2 +1,13 @@
-package racingcar;public class Application {
+package racingcar;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int numberOfCars = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = scanner.nextInt();
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
-  
+
+    @DisplayName("자동차를 생성하라")
+    @Test
+    void create() {
+        Car car = new Car();
+        assertThat(car).isNotNull();
+    }
+
 }
