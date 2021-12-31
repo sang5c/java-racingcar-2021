3fe01329d624eba79280583cb5f19f36d4c004ec
sang5c <skah321@gmail.com>
2021-12-31 23:44:17 +0900

    feat(main): 자동차 수만큼 자동차를 생성하라

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,18 @@ package racingcar;
 
 import racingcar.ui.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         int numberOfCars = InputView.inputCarCount();
         int tryCount = InputView.inputTryCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < numberOfCars; i++) {
+            cars.add(new Car());
+        }
     }
 
 }
